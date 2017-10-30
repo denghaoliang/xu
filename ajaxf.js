793bcbff3a41d7689d7064877ac6069d9ce4c453
denghaoliang <1377196304@qq.com>
2017-10-30 19:54:05 +0800

    19

--- ajaxf.js
+++ ajaxf.js
@@ -8,7 +8,8 @@ $(function(){
 		dataType:'jsonp',
 		processData: false,
 		type:'GET',
-		data:"lng="+jd+"&lat="+wd+"&type=2&dtype=&key=bfaae7122148a72c4541e8ddc2239e99",
+//		data:"lng="+jd+"&lat="+wd+"&type=2&dtype=&key=bfaae7122148a72c4541e8ddc2239e99",
+		data:"lng="+jd+"&lat="+wd+"&type=1&dtype=&key=bfaae7122148a72c4541e8ddc2239e99",
 		success:function(data){
 			$("#guojia").html(data.result.ext.country)
 	        $("#sheng").html(data.result.address)		
