a43529ec2160e4c984e9fcf17c78431072217345
denghaoliang <1377196304@qq.com>
2017-10-30 19:45:16 +0800

    first

---
+++ ajaxf.js
@@ -0,0 +1,28 @@
+
+$(function(){
+	
+	var anniu=$("#button")
+	anniu.click(function(){
+		$.ajax({
+		url:"http://apis.juhe.cn/geo/",
+		dataType:'jsonp',
+		processData: false,
+		type:'GET',
+		data:"lng="+jd+"&lat="+wd+"&type=2&dtype=&key=bfaae7122148a72c4541e8ddc2239e99",
+		success:function(data){
+			$("#guojia").html(data.result.ext.country)
+	        $("#sheng").html(data.result.address)		
+		}
+    
+		
+		
+		})
+  })
+	
+	
+	
+	
+	
+	
+})
+
